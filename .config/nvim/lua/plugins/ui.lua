340fa7a504d6e49dab8bed493f90d3963dc9d52c
4hug4me <re4urnzer0@outlook.com>
2026-01-01 16:03:43 +0800

    update

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -65,4 +65,10 @@ return {
   },
 
   { "akinsho/bufferline.nvim", enabled = false },
+  {
+    "nvim-lualine/lualine.nvim",
+    opts = function(_, opts)
+      opts.options.section_separators = { left = "", right = "" }
+    end,
+  },
 }
